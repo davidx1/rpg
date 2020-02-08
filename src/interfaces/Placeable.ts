cfec4af0d1322bd516eead0acdffe12bf03052e1
David Xu <davidxu1991@gmail.com>
2020-02-08 16:58:14 +1300

    Adding tileset

--- src/interfaces/Placeable.ts
+++ src/interfaces/Placeable.ts
@@ -8,6 +8,18 @@ export enum PlaceableTypes {
   'THINGS',
 }
 
+export enum ThingsSubtypes {
+  'HUT_WOOD_SMALL',
+  'HUT_WOOD_LARGE',
+  'HUT_WOOD_MID1',
+  'HUT_WOOD_MID2',
+  'HUT_TILE_SMALL',
+  'HUT_TILE_LARGE',
+  'HUT_TILE_MID1',
+  'HUT_TILE_MID2',
+  '',
+}
+
 interface Placeable {
   position: Coordinates
   type: PlaceableTypes
